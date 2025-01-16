7d1359cf118b8e83da8f4487af8d3d427b9f9204
Alex Bird <alex.bird@hunterindustries.com>
2025-01-16 07:25:43 -0800

    Update test.mjs to discover and run all *.spec.mjs files

--- test.mjs
+++ test.mjs
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
 import './diyUnit.mjs';
-import './toFizzBuzzString.spec.mjs';
-import './FizzBuzz.spec.mjs';
+import fs from 'node:fs';
+
+// From a web search for "mjs __dirname"
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+    
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// From a web search for "Node 22 recursive find files"
+const files = await fs.readdirSync(__dirname, { recursive: true });
+const tests = files.filter(fileName => fileName.endsWith('.spec.mjs'));
+
+tests.forEach(test => {
+  // From a web search for "mjs dynamic import"
+  import('./' + test);
+});
