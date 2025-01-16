0df47b170c4dbef3414c1234bd0cc42fc87e7c38
Alex Bird <alex.bird@hunterindustries.com>
2025-01-16 07:53:37 -0800

    Temporarily make more tests failing so we can refactor diyUnit with better feedback

--- toFizzBuzzString.mjs
+++ toFizzBuzzString.mjs
@@ -1,8 +1,8 @@
 export function toFizzBuzzString(index) {
   let result = '';
-  if (index % 3 === 0) {
-    result += "Fizz";
-  }
+  //if (index % 3 === 0) {
+    //result += "Fizz";
+  //}
   if (index % 5 === 0) {
     result += "Buzz";
   }
