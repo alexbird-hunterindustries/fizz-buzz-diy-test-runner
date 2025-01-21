828d44b3758b481edbcdbaf119533c24de8aa933
Alex Bird <alex.bird@hunter.global>
2025-01-21 13:01:45 -0800

    Create an empty file for the toFizzBuzzString specification

---
+++ src/toFizzBuzzString.spec.mjs
@@ -0,0 +1,7 @@
+/** toFizzBuzzString
+ * converts a number to a string
+ *   - For most numbers, return the number
+ *   - If it is divisible by 3, return "Fizz" instead
+ *   - If it is divisible by 5, return "Buzz" instead
+ *   - If it is divisible by 3 and 5, return "FizzBuzz" instead
+ */
