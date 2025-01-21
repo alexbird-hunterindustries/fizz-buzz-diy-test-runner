222493ccf6eaee4914f8b4ff58724d844e7b548b
Alex Bird <alex.bird@hunter.global>
2025-01-21 14:44:49 -0800

    Update diyUnit: fix ReferenceError: describe is not defined

--- src/diyUnit.mjs
+++ src/diyUnit.mjs
@@ -1,7 +1,11 @@
-export function expect(actual) {
-    return {
-        toEqual: expected => {
-            console.log({ result: actual === expected ? '✅ PASS' : '❌ FAIL', expected, actual: actual });
-        }
+global.describe = (name, describeBlock) => {
+
+}
+
+function expect(actual) {
+  return {
+    toEqual: expected => {
+      console.log({ result: actual === expected ? '✅ PASS' : '❌ FAIL', expected, actual: actual });
     }
+  }
 }
