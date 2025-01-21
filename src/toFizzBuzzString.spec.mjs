de30ef833ae8f527545cd679f1491cdbbd577563
Alex Bird <alex.bird@hunter.global>
2025-01-21 14:29:34 -0800

    Green: 4 => 4
    It's not a failing test, but I'd like to lock in that behaviour

--- src/toFizzBuzzString.spec.mjs
+++ src/toFizzBuzzString.spec.mjs
@@ -12,4 +12,5 @@ import { expect } from "./diyUnit.mjs";
 expect(toFizzBuzzString(1)).toEqual(1);
 expect(toFizzBuzzString(2)).toEqual(2);
 expect(toFizzBuzzString(3)).toEqual("Fizz");
+expect(toFizzBuzzString(4)).toEqual(4);
 
