8b9020e360bba4572a296bfaf8d105264edef6ac
Alex Bird <alex.bird@hunter.global>
2025-01-21 16:27:32 -0800

    Update diyUnit to have some stats
    Remember to comment out some prod code to show it failing too

--- src/diyUnit.mjs
+++ src/diyUnit.mjs
@@ -5,22 +5,28 @@ global.describe = (describeName, describeBlock) => {
       return {
         toEqual: expected => {
           const pass = actual === expected;
-          results.push({ pass })
-          console.log({ result: pass ? '✅ PASS' : '❌ FAIL', expected, actual: actual });
-        }
-      }
-    }
+          results.push({ pass });
+          console.log({ result: pass ? "✅ PASS" : "❌ FAIL", expected, actual: actual });
+        },
+      };
+    };
     testBlock();
-  }
+  };
   describeBlock();
-  console.log('')
+  console.log("");
   const allPass = results.every(x => x.pass);
-  console.log(allPass ? color.green('PASS') : color.red('FAIL'))
-}
+  const passCount = results.filter(x => x.pass).length;
+  const failCount = results.filter(x => !x.pass).length;
+  console.log(`Ran: ${results.length}`);
+  console.log(`Passed: ${passCount}`);
+  console.log(`Failed: ${failCount}`);
+  console.log("");
+  console.log(allPass ? color.green("PASS") : color.red("FAIL"));
+};
 
 // Color codes come from the Stack Overflow result for "Node console log color"
 // https://stackoverflow.com/a/41407246
 const color = {
   red: text => `\x1b[31m${text}\x1b[0m`,
   green: text => `\x1b[32m${text}\x1b[0m`,
-}
+};
