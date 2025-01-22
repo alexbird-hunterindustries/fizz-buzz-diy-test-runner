e5104a6b59dd5446dadb759cd5f2131d02497d03
Alex Bird <alex.bird@hunter.global>
2025-01-21 16:21:10 -0800

    Update diyUnit to have a pass/fail message

--- src/diyUnit.mjs
+++ src/diyUnit.mjs
@@ -1,14 +1,20 @@
 global.describe = (describeName, describeBlock) => {
+  let allPass = true;
   global.it = (testName, testBlock) => {
     global.expect = actual => {
       return {
         toEqual: expected => {
           const pass = actual === expected;
           console.log({ result: pass ? '✅ PASS' : '❌ FAIL', expected, actual: actual });
+          if (!pass) {
+            allPass = false;
+          }
         }
       }
     }
     testBlock();
   }
-  describeBlock()
+  describeBlock();
+  console.log('')
+  console.log(allPass ? 'PASS' : 'FAIL')
 }
