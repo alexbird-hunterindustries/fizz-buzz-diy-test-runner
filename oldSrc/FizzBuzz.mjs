f0cc8d94e9f46a10492f580b84d106ac2df14238
Alex Bird <alex.bird@hunter.global>
2025-01-22 11:49:02 -0800

    Cleanup: remove oldSrc

--- oldSrc/FizzBuzz.mjs
+++
@@ -1,8 +0,0 @@
-
-export function buildFizzBuzz({ consoleLog, toFizzBuzzString }) {
-  return function fizzBuzz(count) {
-    for (let i = 1; i <= count; i++) {
-      consoleLog(toFizzBuzzString(i))
-    }
-  }
-}
--- oldSrc/main.mjs
+++
@@ -1,9 +0,0 @@
-import { toFizzBuzzString } from './toFizzBuzzString.mjs';
-import { buildFizzBuzz } from './FizzBuzz.mjs';
-
-const fizzBuzz = buildFizzBuzz({
-  toFizzBuzzString,
-  consoleLog: console.log
-});
-
-fizzBuzz(100);
