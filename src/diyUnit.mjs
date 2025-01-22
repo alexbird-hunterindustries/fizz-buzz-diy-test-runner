0240b664c0696fc64cb69505a7ba07bd36298910
Alex Bird <alex.bird@hunter.global>
2025-01-21 16:12:43 -0800

    Update diyUnit to resolve ReferenceError: expect is not defined
    Plugging the existing expect implementation into the describe/it stuff

--- src/diyUnit.mjs
+++ src/diyUnit.mjs
@@ -1,18 +1,17 @@
 global.describe = (describeName, describeBlock) => {
   global.it = (testName, testBlock) => {
+    global.expect = actual => {
+      return {
+        toEqual: expected => {
+          console.log({
+            result: actual === expected ? "✅ PASS" : "❌ FAIL",
+            expected,
+            actual,
+          });
+        },
+      };
+    };
     testBlock();
   };
   describeBlock();
 };
-
-function expect(actual) {
-  return {
-    toEqual: expected => {
-      console.log({
-        result: actual === expected ? "✅ PASS" : "❌ FAIL",
-        expected,
-        actual,
-      });
-    },
-  };
-}
