69da16011a4c449c09e44120d54d48b4d26a11cf
Alex Bird <alex.bird@hunter.global>
2025-01-21 16:22:48 -0800

    Update diyUnit to color the result summary

--- src/diyUnit.mjs
+++ src/diyUnit.mjs
@@ -16,6 +16,12 @@ global.describe = (describeName, describeBlock) => {
     testBlock();
   };
   describeBlock();
-  console.log("");
-  console.log(allPass ? "PASS" : "FAIL");
+  console.log(allPass ? color.green("PASS") : color.red("FAIL"));
+};
+
+// Color codes come from the Stack Overflow result for "Node console log color"
+// https://stackoverflow.com/a/41407246
+const color = {
+  red: text => `\x1b[31m${text}\x1b[0m`,
+  green: text => `\x1b[32m${text}\x1b[0m`,
 };
