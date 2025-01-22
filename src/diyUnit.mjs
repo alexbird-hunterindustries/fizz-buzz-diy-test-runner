bd9d6d0d622e110330af3a40432bc9ba18bc6b01
Alex Bird <alex.bird@hunter.global>
2025-01-21 16:21:10 -0800

    Update diyUnit to have a pass/fail message
    Don't forget to demo what this looks like on failure
    (comment out some implementation code and rerun)

--- src/diyUnit.mjs
+++ src/diyUnit.mjs
@@ -1,4 +1,5 @@
 global.describe = (describeName, describeBlock) => {
+  let allPass = true;
   global.it = (testName, testBlock) => {
     global.expect = actual => {
       return {
@@ -6,10 +7,15 @@ global.describe = (describeName, describeBlock) => {
           const pass = actual === expected;
           const result = pass ? "✅ PASS" : "❌ FAIL";
           console.log({ result, expected, actual });
+          if (!pass) {
+            allPass = false;
+          }
         },
       };
     };
     testBlock();
   };
   describeBlock();
+  console.log("");
+  console.log(allPass ? "PASS" : "FAIL");
 };
