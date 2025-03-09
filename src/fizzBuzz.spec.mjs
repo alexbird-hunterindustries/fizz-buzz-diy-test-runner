a1fca79955bdf164b8e1af174712aa576ebea90e
Alex Bird <alex.bird@hunter.global>
2025-03-08 19:36:53 -0800

    Wrap the tests in an imaginary 'describe' method

--- src/fizzBuzz.spec.mjs
+++ src/fizzBuzz.spec.mjs
@@ -22,13 +22,15 @@ function expect(actual) {
   };
 }
 
-expect(fizzBuzz(1)).toEqual(1);
-expect(fizzBuzz(2)).toEqual(2);
-expect(fizzBuzz(3)).toEqual("Fizz");
-expect(fizzBuzz(4)).toEqual(4);
-expect(fizzBuzz(5)).toEqual("Buzz");
-expect(fizzBuzz(6)).toEqual("Fizz");
-expect(fizzBuzz(10)).toEqual("Buzz");
-expect(fizzBuzz(15)).toEqual("FizzBuzz");
+describe("FizzBuzz", () => {
+  expect(fizzBuzz(1)).toEqual(1);
+  expect(fizzBuzz(2)).toEqual(2);
+  expect(fizzBuzz(3)).toEqual("Fizz");
+  expect(fizzBuzz(4)).toEqual(4);
+  expect(fizzBuzz(5)).toEqual("Buzz");
+  expect(fizzBuzz(6)).toEqual("Fizz");
+  expect(fizzBuzz(10)).toEqual("Buzz");
+  expect(fizzBuzz(15)).toEqual("FizzBuzz");
+});
 
 console.log(!failures ? "ALL PASS" : `${failures} FAILED`);
