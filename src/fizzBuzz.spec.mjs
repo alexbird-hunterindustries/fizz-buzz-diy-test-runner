832eb1c8fa9b403e35c448a32a0cb3819058527d
Alex Bird <alex.bird@hunter.global>
2025-03-08 19:09:17 -0800

    Red 3 => Fizz

--- src/fizzBuzz.spec.mjs
+++ src/fizzBuzz.spec.mjs
@@ -2,3 +2,4 @@ import { fizzBuzz } from "./fizzBuzz.mjs";
 
 console.log("expected 1 => 1, got", fizzBuzz(1));
 console.log("expected 2 => 2, got", fizzBuzz(2));
+console.log("expected 3 => Fizz, got", fizzBuzz(3));
