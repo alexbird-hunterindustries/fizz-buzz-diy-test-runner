b69622522714b1beb4406c68fcd6bb27f07344a6
Alex Bird <alex.bird@hunter.global>
2025-03-08 19:13:26 -0800

    Red 10 => Buzz

--- src/fizzBuzz.spec.mjs
+++ src/fizzBuzz.spec.mjs
@@ -6,3 +6,4 @@ console.log("expected 3 => Fizz, got", fizzBuzz(3));
 console.log("expected 4 => 4, got", fizzBuzz(4));
 console.log("expected 5 => Buzz, got", fizzBuzz(5));
 console.log("expected 6 => Fizz, got", fizzBuzz(6));
+console.log("expected 10 => Buzz, got", fizzBuzz(10));
