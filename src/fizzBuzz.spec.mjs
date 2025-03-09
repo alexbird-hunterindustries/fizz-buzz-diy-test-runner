9762ddcf6b23780a7fbbe8af4491662b5587f176
Alex Bird <alex.bird@hunter.global>
2025-03-08 19:28:19 -0800

    Add a pass/fail indicator

--- src/fizzBuzz.spec.mjs
+++ src/fizzBuzz.spec.mjs
@@ -1,7 +1,12 @@
 import { fizzBuzz } from "./fizzBuzz.mjs";
 
+let failures = 0;
+
 function test(expected, actual) {
   const pass = expected === actual;
+  if (!pass) {
+    failures += 1;
+  }
   console.log(pass ? "✅" : "❌", `expected ${expected}, got ${actual}`);
 }
 
@@ -13,3 +18,5 @@ test("Buzz", fizzBuzz(5));
 test("Fizz", fizzBuzz(6));
 test("Buzz", fizzBuzz(10));
 test("FizzBuzz", fizzBuzz(15));
+
+console.log(!failures ? "ALL PASS" : `${failures} FAILED`);
